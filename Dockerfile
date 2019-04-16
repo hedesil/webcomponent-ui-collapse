1943294692a7988f15e784feb5335d0a6f3d0498
jiglesias <jiglesias@EBP-OR7506425.cosmos.es.ftgroup>
2019-04-16 17:26:52 +0200

    nueva distribucion de la configuracion del Dockerfile y Nginx

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
 FROM nginx
 
-COPY index.html /usr/share/nginx/html
-COPY bundle.js /usr/share/nginx/html
+COPY elements/index.html /usr/share/nginx/html/
+COPY elements/bundle.js /usr/share/nginx/html/
 
+# ssl autosign certs for testing
+COPY etc/ssl/certs/ssl-cert-snakeoil.pem /etc/ssl/certs/
+COPY etc/ssl/private/ssl-cert-snakeoil.key /etc/ssl/private/
+
+# nginx configuration files
+COPY etc/nginx/conf.d/default.conf /etc/nginx/conf.d/
+COPY etc/nginx/nginx.conf /etc/nginx/
--- buildnrun.sh
+++ buildnrun.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+echo Docker build...
+#sudo docker build --rm --no-cache -t `grep name elements/package.json |cut -d"\"" -f4` .
+sudo docker build -t `grep name elements/package.json |cut -d"\"" -f4` .
+echo Build success
 
-sudo docker build --rm --no-cache -t `grep name elements/package.json |cut -d"\"" -f4` .
-
-sudo docker run -p 9090:80 `grep name elements/package.json |cut -d"\"" -f4` &
+echo Docker run...
+#sudo docker run -p 9090:443 -it `grep name elements/package.json |cut -d"\"" -f4` /bin/bash
+sudo docker run -p 9090:443 `grep name elements/package.json |cut -d"\"" -f4` 
+echo Done!
